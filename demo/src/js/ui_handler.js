eeb66eb8f3d7bcbceac13774f86a65aca1ddd6f3
DongHyun Yang <dh.yang@alcherainc.com>
2023-05-02 18:44:59 +0900

    [KYC-2745][Demo] console log 자동 삭제 되도록 gulp babel 적용

--- demo/src/js/ui_handler.js
+++ demo/src/js/ui_handler.js
@@ -18,15 +18,15 @@ changeEvent.initEvent("change", true, false);
 // })
 
 paramBox.addEventListener('click', () => {
-    document.querySelector('#param .customer--radio-check').classList.add('checked');
-    document.querySelector('#use_input_ui .customer--radio-check').classList.remove('checked');
+    document.querySelector('#param .custom--radio-check').classList.add('checked');
+    document.querySelector('#use_input_ui .custom--radio-check').classList.remove('checked');
     userinfoTypeSelect.options[0].selected = true;
     userinfoTypeSelect.dispatchEvent(changeEvent);
     userinfoDivision.style.display = 'block'
 });
 useInputUiBox.addEventListener('click', () => {
-    document.querySelector('#use_input_ui .customer--radio-check').classList.add('checked');
-    document.querySelector('#param .customer--radio-check').classList.remove('checked');
+    document.querySelector('#use_input_ui .custom--radio-check').classList.add('checked');
+    document.querySelector('#param .custom--radio-check').classList.remove('checked');
     userinfoTypeSelect.options[1].selected = true;
     userinfoTypeSelect.dispatchEvent(changeEvent);
     userinfoDivision.style.display = 'none';
